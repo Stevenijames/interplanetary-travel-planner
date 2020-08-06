1c0f7aaeaf87a9d9dbdd7ad22e9be6e4be9b29a5
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-05 21:56:26 -0600

    finalizing Travis-CI and Coveralls integration

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 require("dotenv").config();
+
+function awesomeAdd(num1, num2) {
+  return num1 + num2;
+  console.log("awesome add!!!");
+}
+
+module.exports = { awesomeAdd: awesomeAdd }
--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
-const { should, expect, assert } = require("chai");
-
+const { expect } = require("chai");
+const { awesomeAdd } = require("../server");
 
 describe("Array", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
       expect([1, 2, 3].indexOf(5)).to.equal(-1);
+    });
+  });
+});
+
+describe("Functions", function () {
+  describe("awesomeAdd()", function () {
+    it("should awesomely add values!", function () {
+      expect(awesomeAdd(1, 5)).to.equal(6);
     });
   });
 });
