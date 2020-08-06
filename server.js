f48ff8d8487b2ebaf235db02f477c405a9fe4fd4
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-06 11:00:36 -0600

    demoing Travis and tests

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 
 function awesomeAdd(num1, num2) {
   return num1 + num2;
-  console.log("awesome add!!!");
 }
 
-module.exports = { awesomeAdd: awesomeAdd }
+function awesomeSubtract(num1, num2) {
+  return num1 - num2;
+}
+
+module.exports = { awesomeAdd, awesomeSubtract };
