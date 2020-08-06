e07d65786ae83638fb77f41ada93c99378731a9a
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-05 20:52:26 -0600

    writing test suite

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const { should, expect, assert } = require("chai");
+
+
+describe("Array", function () {
+  describe("#indexOf()", function () {
+    it("should return -1 when the value is not present", function () {
+      expect([1, 2, 3].indexOf(5)).to.equal(-1);
+    });
+  });
+});
